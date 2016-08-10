241521849451c6e95b1101404c28223a879c5c7d
Steven Covington <dlinetackle@yahoo.com>
2016-08-10 21:21:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-function theBeatlesPlay (musicians, instruments){
-  var emptyarray = {
-  for (let i = 0; i < 4; i++)
-  if (i <== 4){
-  musicians.push (${musicians} plays ${instruments})
+function theBeatlesPlay(musicians, instruments) {
+	var array = []
+	for (var i = 0; i < musicians.length; i++) {
+		array[i] = musicians[i] + " plays " + instruments[i]
+	}
+	return array;
 }
+function johnLennonFacts(array) {
+  var i = 0;
+  var arr = [];
+  while (i < array.length) {
+    arr[i] = array[i] +"!!!";
+    i += 1;
+  }
+  return arr;
 }
-return emptyarray
+function iLoveTheBeatles(n) {
+var array = []
+  if (n === 17) {
+    array = "I love the Beatles!";
+  } else {
+    do {
+      array.push('I love the Beatles!');
+      n -= 1;
+    } while (n >= 0)
+  }
+  return array;
 }
