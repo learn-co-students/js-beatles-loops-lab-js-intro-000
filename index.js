15e315d3f1a5eedd871919ddb6f8613ed0dfdb7d
Steven Covington <dlinetackle@yahoo.com>
2016-08-10 20:45:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function theBeatlesPlay (musicians, instruments){
-  var emptyarray = {}
-
+  var emptyarray = {
+  for (let i = 0; i < 4; i++)
+  if (i <== 4){
+  musicians.push (${musicians} plays ${instruments})
+}
+}
+return emptyarray
 }
