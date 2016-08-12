0ab2b2b834aefa8f99fc28ff5fdfe911dd346e08
Sang Eun Park <sep795@gmail.com>
2016-08-12 05:27:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++)
+  {array[i] = musicians[i] + ' plays ' + instruments[i]}
+return array
+}
+
+function johnLennonFacts(facts) {
+var array = [];
+var i = 0;
+  while (facts[i])
+  {array[i] = facts[i] + "!!!"; i++}
+return array
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {array.push("I love the Beatles!"); n++}
+    while (n < 15)
+return array
+}
