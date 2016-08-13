4f524d1b6b0d8c0555cd943b63b62e98df58e0d8
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-13 23:58:24 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ function johnLennonFacts(facts){
   var i = 0;
   while(i < facts.length){
     facts[i] = facts[i] + "!!!";
+    i++;
   };
   return facts;
+};
+
+function iLoveTheBeatles(number){
+  var myArr = [];
+  do{
+    myArr.push("I love the Beatles!");
+    number+=1;
+  }while(number<15);
+
+  return myArr;
 };
