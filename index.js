74851e6ccdb10de3670830bd2c9ba99bce8f9191
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-13 23:33:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function theBeatlesPlay(musicians, instruments){
   var stringArray = [];
-  for (var i=0; i<(musicians.length-1);i++){
+  for (var i=0; i<(musicians.length);i++){
     stringArray.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+
   return stringArray;
+};
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while(i < facts.length){
+    facts[i] = facts[i] + "!!!";
+  };
+  return facts;
 };
