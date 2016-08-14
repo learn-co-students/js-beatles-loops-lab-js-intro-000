b1af5e659723443ae3f66b54071ce176d2168f9b
Victor  Tsang <victorwltsang@gmail.com>
2016-08-14 20:07:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function theBeatlesPlay(musicians,instruments){
+  var plays = [];
+
+  for(var x in musicians){
+    plays.push(`${musicians[x]} plays ${instruments[x]}`);
+  }
+
+  return plays;
+
+}
+
+function johnLennonFacts(facts) {
+  const shoutedFacts = [];
+
+  let i = 0;
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`);
+    i++;
+  }
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  if(num === 7){
+    for(var i  = 0; i < num; i++){
+      arr.push("I love the Beatles!");
+    }
+    return arr;
+  }
+  if(num === 17){
+    for(var i  = 0; i < 1; i++){
+      arr.push("I love the Beatles!");
+    }
+    return arr;
+  }
+
+}
