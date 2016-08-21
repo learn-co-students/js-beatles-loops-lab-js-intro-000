c0b6653f8b744c287572cfe75755c71a14c97f4e
syed shaheeruddin <shaheeruddinsyeds@gmail.com>
2016-08-21 16:04:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function theBeatlesPlay(musicians, instruments){
+  var shaheer=[];
+for(let i=0;i<musicians.length;i++){
+shaheer.push(`${musicians[i]}` +" plays "+ `${instruments[i]}`)
+}
+return shaheer
+}
+
+
+/*function johnLennonFacts(facts){
+  var i=4;
+        while(i){
+facts=console.log(facts[i], "!!!");
+i--;
+}
+return facts
+}*/
+
+function johnLennonFacts(facts){
+  var i=4;
+        while(i){
+facts=console.log(facts[i], "!!!");
+i--;
+}
+return facts
+}
+
+
+function iLoveTheBeatles(number){
+  var number=[];
+  var counter=0;
+  do{
+number.push("I love the Beatles!")
+counter++
+  }while(counter<15)
+  return number
+}
