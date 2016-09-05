63b5cbaa2a82abd074827a78d8bd4d5b92a9f879
David Ganthier <david.ganthier@gmail.com>
2016-09-05 22:26:26 +0000

    Done.

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ return newArray;
 
 /* Beatles Loops johnLennonFacts returns an array of strings with exclamation
 points: ReferenceError: johnLennonFacts is not defined */
-
-
 function johnLennonFacts(array) {
   var i = 0
   while (array.length > i) {
@@ -21,17 +19,17 @@ function johnLennonFacts(array) {
 return array;
 }
 
-
 /* Beatles Loops iLoveTheBeatles returns an array of 'I love the Beatles!' 8
 times when passed the parameter 7 : ReferenceError: iLoveTheBeatles is not
-defined */
+defined
 
-
-function iLoveTheBeatles(int) {
+Beatles Loops iLoveTheBeatles returns an array of 'I love the Beatles!' once
+when passed the parameter 17: ReferenceError: iLoveTheBeatles is not defined  */
+function iLoveTheBeatles(n) {
   var newArray = []; // Empty Array.
   do {
     newArray.push("I love the Beatles!");
-    return newArray += 1;
-  } while (newArray <= 8);
+    n++;
+  } while (n < 15);
 return newArray;
 }
