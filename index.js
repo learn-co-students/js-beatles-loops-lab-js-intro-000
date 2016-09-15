8957051070afba62482b1c50295d3ad4f0e28260
Fernando Gomez <fhergomez37@gmail.com>
2016-09-15 02:28:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
-  for (let i = 0, l = musicians.length; i < array.length;i++){
+  for (let i = 0, l = musicians.length; i < l;i++){
     array.push(`"${musicians} plays ${instruments}"`)
   }
   return array;
