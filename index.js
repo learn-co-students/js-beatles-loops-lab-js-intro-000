76b01811b6d50f7719a28f785a7a5b6e99c40e2a
Fernando Gomez <fhergomez37@gmail.com>
2016-09-15 05:18:36 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ function johnLennonFacts(facts){
   var array = [];
   let i = 0
   while (i < facts.length) {
-    array.push(`${facts}[i]!!!`);
+    array.push(`${facts[i]}!!!`);
     i++;
   }
   return array;
 }
 
 function iLoveTheBeatles(number){
-
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+  } while (number === 7 && number === 17);
+  return array;
 }
