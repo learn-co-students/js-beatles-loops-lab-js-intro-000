fbc38834966acd888252ff1180cea53e7331693b
Fernando Gomez <fhergomez37@gmail.com>
2016-09-15 02:22:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (var i = 0; i < array.length;i++){
+    array.push(musicians + "plays" + instruments)
+  }
+  return array;
+}
