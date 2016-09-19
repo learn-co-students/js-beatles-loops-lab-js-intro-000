45c69fe7fcbb52606374c04e5125c97b27c7123e
Ben Fletcher <bfletch@gmail.com>
2016-09-19 16:38:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musicians, instruments) {
+  var tempArr = [];
+
+  for (var i = 0; i < musicians.length; i += 1) {
+    tempArr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+
+  return tempArr;
+}
+
+function johnLennonFacts(facts) {
+  var i = factArr.length;
+  var newFacts = [];
+
+  while (i--) newArr.push(facts + "!!!");
+
+  return newFacts;
+}
+
+function iLoveTheBeatles(n) {
+  var arr = [];
+
+  do {
+    arr.push("I love the Beatles!");
+  } while (++n < 15);
+
+  return arr;
+}
