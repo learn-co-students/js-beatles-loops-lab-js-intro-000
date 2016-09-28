bc5f9151ced0cb56d984d45021b12b743d9e3060
crisam degracia <crisamdegracia@gmail.com>
2016-09-28 10:44:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musicians,instruments) {
+var array = []
+for (array.lenght = 0 ; array.lenght < 4  ; array.lenght++)
+{  var xx = "plays"
+  if ( array.lenght === 0 ){
+ array.push(`${musicians[0]} ${xx} ${instruments[0]}`)
+}
+else if (array.lenght === 1 ){
+array.push(`${musicians[1]} ${xx} ${instruments[1]}`)
+}
+else if (array.lenght === 2 ){
+array.push(`${musicians[2]} ${xx} ${instruments[2]}`)
+}
+else if (array.lenght === 3 ){
+array.push(`${musicians[3]} ${xx} ${instruments[3]}`)
+}
+}
+return array
+}
+
+function johnLennonFacts(array){
+
+
+  while (array.lenght === 0){
+
+    return `${array[0]} ${"!!!"}`
+  }
+}
