15fffd575a8b97ddd4491e659cd2cfe48cdd07bb
Robert Hitt <robhitt@gmail.com>
2016-11-06 02:14:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicianArray, instrumentsArray) {
+  var newArr = []
+
+  for (var i = 0 ; i < musicianArray.length ; i++ ) {
+    newArr.push(`${musicianArray[i]} plays ${instrumentsArray[i]} `)
+  }
+}
