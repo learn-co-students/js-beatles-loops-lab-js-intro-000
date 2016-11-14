f8a019ef384a26c4ce69d4dcf2be8e41d8231257
Lassiter Gregg <lassitergregg@gmail.com>
2016-11-14 02:41:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 // theBeatlesPlay case
-var musicians = [
-  'John Lennon',
-  'Paul McCartney',
-  'George Harrison',
-  'Ringo Starr'
-]
-var instruments = [
-  'Guitar',
-  'Bass Guitar',
-  'Lead Guitar',
-  'Drums'
-]
-var strings = []
+
 function theBeatlesPlay(musicians, instruments){
+    var strings = []
       var i = musicians.length && instruments.length
       for (i = 0; i < musicians.length && i < instruments.length; i++){
         strings.push(musicians[i] + ' plays ' + instruments[i])
       }
     return strings
 }
-// johnLennonFacts case
+//johnLennonFacts case
 function johnLennonFacts(facts){
+  var facts2 = []
   var counter = 0;
   while (counter < facts.length){
-    facts.push(facts[0] + "!!!")
+    facts2.push(facts[counter] + "!!!")
+    counter++
   }
-  return facts
+  console.log(facts2)
+  return facts2
+}
+// iLoveTheBeatles case
+function iLoveTheBeatles(i){
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    i++;
+  }
+  while (i < 15){
+    return array;
+  };
+
 }
