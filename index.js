03db62995f00f689b9bbf66c7a98199246d14b3b
Feiran Wang <wangfeiranpku@163.com>
2016-12-19 23:48:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians,instruments) {
+  var array =[];
+  for (var i=0; i<musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array
+}
+function johnLennonFacts(facts) {
+  var array = [];
+  for (var i = 0; i < facts. length; i++) {
+    array.push (facts[i] + "!!!")
+  }
+  return array
+}
+function iLoveTheBeatles(n) {
+  var array=[];
+  do{
+    array.push("I love the Beatles!")
+    n = n +1
+  }
+  while(n<15)
+  return array
+}
