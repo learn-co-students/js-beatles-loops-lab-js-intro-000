36dbb32e9aaf1a896ab60c3a82f78829c05a1d38
Philip Sailer <philsailer@gmail.com>
2017-02-17 03:34:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians, instruments) {
+  var players = []
+  for (let i = 0; i < musicians.length; i++) {
+    players.push(musicians[i] + " plays " + instruments[i])
+  }
+  return players
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = []
+  for (let i = 0; i < facts.length; i++) {
+    newFacts[i] = facts[i] + "!!!"
+  }
+  return newFacts
+}
+
+function iLoveTheBeatles(num) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+  } while (++num < 15)
+  return array
+}
