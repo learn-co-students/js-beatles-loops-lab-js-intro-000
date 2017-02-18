cc766f2ee0f21b162bfcf8c8d856112d23834cc2
Jessica Blake <jcblake14@gmail.com>
2017-02-18 17:16:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = new Array;
+
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  for (var i = 0; i < facts.length; i++) {
+    facts[i] = `${facts[i]}!!!`
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var array = new Array;
+  do {
+    array.push("I love the Beatles!");
+    console.log(++number);
+  } while (number < 15);
+  return array;
+}
