f8afb2c00f2c515ecfe9855f5304752ec8795254
Jacob Anderson <jacob.colin.anderson@icloud.com>
2017-03-06 04:19:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function theBeatlesPlay(musicians, instruments) {
+   var fabFour = [];
+    for (var i=0; i < musicians.length; i++) {
+             fabFour.push(musicians[i] + " plays " + instruments[i]);
+   }
+    return fabFour;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
+}
+
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
+}
