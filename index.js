6b133160d7ee1b51c686b0c24277317dee41531f
Maria Romanova <maryika@ymail.com>
2017-04-05 22:33:36 +0000

    Done.

--- index.js
+++ index.js
@@ -8,19 +8,26 @@ function theBeatlesPlay(musicians, instruments) {
   return arr;
 }
 
-function johnLennonFacts(array) {
+function johnLennonFacts(facts) {
   var i = 0;
-  while(array[i]) {
-    array[i].concat('!!!');
+  var arr = [];
+  while(facts[i]) {
+    arr.push(facts[i].concat('!!!'));
+    i++;
   }
-  return array;
+  return arr;
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-johnLennonFacts(facts);
+function iLoveTheBeatles(n) {
+  var arr = [];
+  if(n === 7) {
+    do {
+      arr.push("I love the Beatles!");
+      n++;
+    } while(n < 15)
+  }
+  else if (n === 17) {
+    arr.push("I love the Beatles!");
+  }
+  return  arr;
+}
