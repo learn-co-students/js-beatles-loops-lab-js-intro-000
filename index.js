73930834c237c596db5a10a3a29436b941855aaf
Eleanor Thomas <eleanor.thomas.res@gmail.com>
2017-04-05 03:15:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = []
+  var array = new Array
   for (let i = 0; i < musicians.length; i++) {
-    array.push("${musicians[i]} plays ${instruments[i]}")
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array = new Array
+  while (facts.length > 0) {
+    array.push(facts.shift() + "!!!")
+  }
+  return array
+}
+
+function iLoveTheBeatles(n) {
+  var array = new Array
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while (n < 15)
   return array
 }
