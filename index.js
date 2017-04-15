665063e66531838d2af0b627585acd27d436ebd6
sungyun kim <kimsungyun7@hotmail.com>
2017-04-15 22:57:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i])
+  }
+  return arr;
+}
+
+   const facts = [
+   "He was the last Beatle to learn to drive",
+   "He was never a vegetarian",
+   "He was a choir boy and boy scout",
+   "He hated the sound of his own voice"
+ ];
+
+ function johnLennonFacts(facts) {
+   var arr = [], i = 0;
+
+   while (i < facts.length) {
+     arr.push(facts[i] + "!!!")
+     i++
+   }
+   return arr;
+ }
+
+ function iLoveTheBeatles(number) {
+   var arr = [];
+
+   do {
+     arr.push("I love the Beatles!")
+     number++
+   } while (number < 15);
+   return arr;
+ }
