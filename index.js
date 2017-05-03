738a2f10c6e311a1440a2b8e47fd1f4ac3f0356d
Stephen Harding <stephenharding79@gmail.com>
2017-05-03 21:07:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < 4; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+  return array
+  }
+
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
+
+  function johnLennonFacts(facts) {
+    let countup = 0;
+    var array = []
+    while (countup < facts.length) {
+      array.push(`${facts[countup]}!!!`)
+      countup++
+}
+return array
+  }
+
+function iLoveTheBeatles(number) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    number--
+  } while (number > -1 && number < 15);
+  return array
+}
