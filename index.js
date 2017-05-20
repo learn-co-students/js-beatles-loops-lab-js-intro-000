34ef156910333c94570d9a36248006d591dcd992
Manuel Felix <jumanji.felix@gmail.com>
2017-05-20 11:41:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+
+  for (let i = 0; i < musicians.length; i++){
+    array.push( `${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return array
+}
+
+function johnLennonFacts(facts){
+
+  let i=0
+  while (i < facts.length) {
+    facts[i] += '!!!'
+    i++
+  }
+
+  return facts
+}
+
+function iLoveTheBeatles(n){
+  var lovethem = []
+  do{
+    lovethem.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+
+  return lovethem
+}
