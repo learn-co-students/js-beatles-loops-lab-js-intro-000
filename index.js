7e415a6e1585b4d3d2895fdcc5b35aa03b3724fc
Máté Rajnai <ramate.rajnai@gmail.com>
2017-06-05 11:54:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay (musicians, instruments) {var info = [];
+                                  for (var i = 0; i < musicians.length; i++)
+                                      {info.push(musicians[i] + ' plays ' + instruments[i])};
+                                    return info}
+
+function johnLennonFacts(array) {var i = 0; while (array[i] < array.length) {return array[i] + '!!!'}}
+
+function iLoveTheBeatles(number) {var love = []; do {love.push('I love the Beatles!'); ++ number}
+                                                  while (number < 15);
+                                      return love}
