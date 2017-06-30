c19db2ce45217d901c2099a669240d15b4c89bb2
Dani Schuhman <dani8439@aol.com>
2017-06-30 18:36:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-function theBeatlesPlay(musicians, instruments) {
-	var array = []
-	for (var i = 0; i < musicians.length; i++) {
-		array.push(`${musicians[i]} plays ${instruments[i]}`)
-	}
-	return array
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for(var index = 0; index < musicians.length; index++){
+    array.push(`${musicians[index]} plays ${instruments[index]}`);
+  }
+  return array;
+
 }
 
 const facts = [
-	"He was the last Beatle to learn to drive",
-	"He was never a vegetarian",
-	"He was a choir boy and boy scout",
-	"He hated the sound of his own voice"
-]
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
-function johnLennonFacts(facts) {
-	for (var i = 0; i < facts.length; i++) {
-		facts[i] = facts[i] + "!!!"
+function johnLennonFacts(facts){
+	for(var index = 0; index < facts.length; index++){
+		facts[index] = facts[index] + "!!!";
 	}
-	return facts
+	return facts;
 }
 
 function iLoveTheBeatles(n) {
