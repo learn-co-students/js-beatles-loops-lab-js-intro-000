e7c0853b4f71f7b153bbf82ac755fb41395d0ae0
Niles Stanton <stantonniles@gmail.com>
2017-08-08 19:24:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for(var i = 0; i < musicians.length; i++){
+    array.push(musicians[i]+ " plays " + instruments[i])
+  }
+  return array
+}
+function johnLennonFacts(array){
+  var i = 0
+  var newArray = []
+  while(i < array.length){
+    newArray.push(array[i] + "!!!")
+    i++
+  }
+  return newArray
+}
+
+function iLoveTheBeatles(n) {
+  var array = []
+  do{
+    array.push('I love the Beatles!')
+    n++
+  }while(n < 15)
+  return array
+}
