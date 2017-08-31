732842e54e6746dd004585083a879da5a4e7831e
Steven Oladipo <stevenboladipo@gmail.com>
2017-08-31 22:26:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+function theBeatlesPlay(musicians , instruments){
+  var array = [];
+for (let i = 0; i < 4; i++)
+  array.push("John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums")
+  return array;
+  }
