7f699d827210d15aee761b6a829b533bd6f09ebe
Steven Oladipo <stevenboladipo@gmail.com>
2017-09-01 21:46:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians , instruments){
   var array = [];
-for (let i = 0; i < 5; i++)
+for (let i = 0; i < 1; i++)
   array.push("John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums")
   return array;
   }
