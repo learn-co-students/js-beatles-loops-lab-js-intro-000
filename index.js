0342ab37700688cb484b080d4ca7e4431bba5c3c
Steven Oladipo <stevenboladipo@gmail.com>
2017-09-01 22:30:34 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ for (let i = 0; i < 1; i++)
   array.push("John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums")
   return array;
   }
+
+function johnLennonFacts(facts){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+while (facts < 5) {
+array.push("!!!");
+  facts++;
+    return array;
+}
+  }
