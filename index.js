f334bb72583dc040456b7b277ce32591cee9a7c4
Jacob Koo <gobleskoo@gmail.com>
2017-09-03 15:01:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (var i = 0; i < 4; i++) {
+  for (let i = 0; i < musicians.length; i++) {
     array[i] = `${musicians[i]} plays ${instruments[i]}`
   }
   return array
