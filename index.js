3260e34e287b2223a79e19a35d99ce74105a0d78
Mark Drake <mdrake@digitalocean.com>
2018-03-03 18:19:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+  var phrases = [];
+  for (var i = 0; i < musicians.length; i++) {
+    phrases.push(musicians[i] + " plays " + instruments[i]);
+  }  
+  return phrases;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(x) {
+  var empty = []
+    do {
+      empty.push("I love the Beatles!");
+      x += 1
+    } while (x < 15) {
+    return empty
+  }
+}
