524923f170602c0d99caa397746ff3dd7e2106c8
Supui Lam <supuilam@gmail.com>
2018-07-06 20:11:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+let theBeatlesPlay = (musicians, instruments) => {
+  let arr = []
+  for (let i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr
+}
+
+let johnLennonFacts = facts => {
+  let i = 0
+  while (i < facts.length) {
+    facts[i] += '!!!'
+    i++
+  }
+  return facts
+}
+
+let iLoveTheBeatles = num => {
+  let arr = []
+  let i = 0
+  do {
+    arr.push('I love the Beatles!')
+    i++
+  } while(i <= num && num < 15)
+  
+  return arr
+}
