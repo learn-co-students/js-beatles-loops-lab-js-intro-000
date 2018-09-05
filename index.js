51f0a0672021cd8b44710ea5eb19fde7dbd4dead
david.vega@snhu.edu <david.vega@snhu.edu>
2018-09-05 00:17:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  for (let i = 0; i < musicians.length; i++){
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArray;
+}
+
+function johnLennonFacts(facts){
+  var factsArray = [];
+  var x = 0
+  while (x < facts.length) {
+    factsArray.push(facts[x] + "!!!");
+    x++;
+  }
+  return factsArray;
+}
