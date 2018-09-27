1b59ad234d3a081b4aaeaa11ab2eec3e02164350
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-27 18:56:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var sentences = []
+  for (var i = 0; i < musicians.length; i++) {
+    var sentence = `${musicians[i]} plays ${instruments[i]}`
+    sentences.push(sentence)
+  }
+  return sentences
+}
+
+function johnLennonFacts(facts) {
+  var ex_facts = []
+  var i = 0
+  while (i < facts.length) {
+    ex_facts.push(facts[i] + "!!!");
+    i ++
+  }
+  return ex_facts
+}
+
+function iLoveTheBeatles(num) {
+  var sentences = []
+  var i = num
+  if (i > 15) {
+    sentences.push("I love the Beatles!")
+  } else {
+    do {
+      sentences.push("I love the Beatles!")
+      i += 1
+    } while (i < 15);
+  }
+  return sentences
+}
