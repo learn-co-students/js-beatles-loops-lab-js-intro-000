7c12ea97b4a27e66b363beb7ab5c9da1e3836d3a
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-17 11:43:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var empty = []
+  for (var i = 0; i < musicians.length; i++) {
+    empty.push(musicians[i] + ` plays ` + instruments[i])
+  }
+  return empty
+}
+function johnLennonFacts(facts){
+var exclamation = []
+var i = 0
+  while (i < facts.length) {
+    exclamation.push(facts[i] + `!!!`)
+    i++
+  }
+  return exclamation
+}
+function iLoveTheBeatles(n){
+  var love = [];
+  do {
+    love.push("I love the Beatles!")
+    n++
+  } while (n < 15);
+  return love
+}
