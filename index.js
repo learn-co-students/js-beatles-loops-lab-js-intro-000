9a1a01d4096bb90d2760565d46111bc7b549bcf1
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2018-10-20 03:39:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const beatles = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatles;
+}
+
+function johnLennonFacts(facts) {
+  const unknownFacts = [];
+
+  let i = 0;
+  while (i < facts.length) {
+    unknownFacts.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return unknownFacts;
+}
+
+function iLoveTheBeatles(n) {
+  const empty = [];
+
+  do {
+    empty.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return empty;
+}
