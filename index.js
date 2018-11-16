2ce769aa6c3e88a616104256f821de7d077b0e18
andy reyes <11710reyes@gmail.com>
2018-11-16 00:26:20 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var excitedFacts = [];
+  var i = 0;
+  while (facts.length > i) {
+    excitedFacts.push(facts[i] + '!!!')
+    i++;
+  }
+  return excitedFacts
+}
+
+function iLoveTheBeatles (number) {
+  let array = [];
+  do { array.push('I love the Beatles!')
+    number++
+  }
+  while (number < 15);
+  return array
+  
+}
