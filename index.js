6397e8ea314a2508598a35a9ad50fd032e342c3f
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-26 04:57:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}// add solution here
+
+function johnLennonFacts(arrayOfFacts) {
+  var i = 0
+  while (i < arrayOfFacts.length) {
+    arrayOfFacts[i] += "!!!"
+    i++
+  }
+  return arrayOfFacts
+}
+
+function iLoveTheBeatles(n) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+   return array
+}
