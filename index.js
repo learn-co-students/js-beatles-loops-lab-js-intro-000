14cb2dda0e11b4ddb618895d7d8c53c4b4141c01
Ellie Reid <reides@dukes.jmu.edu>
2018-12-06 20:20:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+function theBeatlesPlay(muscians, instruments)
+{
+  var array = []
+  for (let i = 0, a = muscians.length; i < a; i++)
+  {
+    array.push(`${muscians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(array)
+{
+  var newarray2 = []
+
+    var i = 0
+    while (i < array.length) {
+      newarray2.push(`${array[i]}!!!`)
+      i++
+    }
+    return newarray2
+
+}
+
+function iLoveTheBeatles(number)
+{
+  var newarray3 = []
+
+  do {
+    newarray3.push("I love the Beatles!")
+    number++
+  } while (number < 15);
+
+  return newarray3
+}
