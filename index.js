60e0dadec1b49823370f43b1d541ac503afe44b8
Yingqi Chen <chanwk18@hotmail.com>
2019-01-18 07:27:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array=new Array();
+  for(var i=0;i<musicians.length;i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }return array;
+}
+
+function johnLennonFacts(array){
+  var i=0;
+  var array2=new Array();
+  while (i<array.length){
+    array2.push(`${array[i]}!!!`);
+    i++;
+  }return array2;
+}
+
+function iLoveTheBeatles(num){
+  var i=num;
+  var array=new Array();
+  do{
+    array.push("I love the Beatles!");
+    i++;
+  }while(i<15);return array;
+}
