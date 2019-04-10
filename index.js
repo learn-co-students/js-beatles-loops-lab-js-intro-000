814a6820059a58718fc114794a210a98d9817f41
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-04-10 07:23:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(array1,array2) {
+  var empty = [];
+  for (let i=0; i< array1.length;i++){
+    empty.push(`${array1[i]} plays ${array2[i]}`)
+  }
+  return empty;
+}
+function johnLennonFacts(array3) {
+  var i=0;
+  var empty= [];
+  while(i<array3.length) {
+    empty.push(array3[i] + "!!!")
+    i++;
+  }
+  return empty;
+}
+function iLoveTheBeatles(number) {
+  var newarray = [];
+  do{number++;
+    newarray.push("I love the Beatles!");
+  }while(number<15);
+  return newarray;
+}
