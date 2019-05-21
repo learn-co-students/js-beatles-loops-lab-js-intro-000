a92c44f74ae69d26893f3b4296aa868bd766fddb
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-21 15:55:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// add solution here
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
@@ -25,3 +24,4 @@ function iLoveTheBeatles(num) {
   } while (num < 15);
   return array;
 }
+
