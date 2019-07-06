5979fb2c2de201ec0eec786c7959471e05fa85a6
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-06 12:04:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+var theBeatlesPlay = (musicians, instruments) => {
+  var resultArray = []
+
+  for (let i = 0; i < musicians.length; i++) {
+    resultArray[i] = musicians[i] + " plays " + instruments[i]
+  }
+
+  return resultArray
+}
+
+var johnLennonFacts = (facts) => {
+  let i = 0
+  var exclamations = []
+
+  while (i < facts.length) {
+    exclamations[i] = facts[i]+"!!!"
+    i++
+  }
+
+  return exclamations
+}
+
+var iLoveTheBeatles = (number) => {
+  var loveArray = []
+
+  do {
+    loveArray.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+
+  return loveArray
+}
