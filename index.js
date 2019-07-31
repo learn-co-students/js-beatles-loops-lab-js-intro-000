f123b313f4341800221d9d0d1f52348aade557df
Erick Melendez <erickmlndz@protonmail.com>
2019-07-31 19:16:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+  function theBeatlesPlay(musicians, instruments) {
+    const array = []
+
+    for (let i = 0, l = musicians.length; i < l; i++) {
+      array.push(`${musicians[i]} plays ${instruments[i]}`);
+      }
+    return array
+}
+
+  function johnLennonFacts(facts) {
+    const exclamation = []
+    let i = 0
+
+    while (i < facts.length) {
+      exclamation.push(`${facts[i]}!!!`);
+      i++;
+  }
+    return exclamation
+}
+
+function iLoveTheBeatles(num) {
+  const fan = []
+
+  do {
+    fan.push('I love the Beatles!');
+    num++;
+  } while (num < 15);
+  return fan;
+}
