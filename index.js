ad30b5f8368a01377a8829d6727c35228b727a68
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-08-05 21:40:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+function johnLennonFacts(facts) {
+  const allFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    allFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return allFacts
+}
+
+function iLoveTheBeatles(n) {
+  const love = []
+
+  do {
+    love.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+  return love
+}
