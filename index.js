00fd40a46b1e877e87271ac7385efe3497152544
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-09-01 22:04:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musiciansArray, instrumentsArray){
+  var mixUp = []
+  for (var i = 0; i < musiciansArray.length; i++){
+    mixUp.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`)
+  }
+  return mixUp
+}
+
+function johnLennonFacts(array){
+  var i = 0
+  var newArray = []
+  while (i < array.length){
+    newArray.push(`${array[i]}!!!`)
+    i = ++i
+  }
+  return newArray
+}
+
+function iLoveTheBeatles(num){
+  var newArray = []
+  do {
+    newArray.push('I love the Beatles!')
+    num = ++num
+  } while (num < 15)
+  return newArray
+}
