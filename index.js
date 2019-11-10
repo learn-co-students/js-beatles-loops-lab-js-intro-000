d07e8cf823a4ebfbac0482e0d2a7f3b8e5ab7042
giorgi <giorgikutubidze@gmail.com>
2019-11-10 01:41:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(arrofmus,arrofinst){
+  var emarray = [];
+  for(var i = 0;i < arrofmus.length;i++){
+    emarray.push(arrofmus[i] +' plays '+arrofinst[i]);
+  }
+  return emarray;
+}
+function johnLennonFacts(facts){
+var array1 = [];
+  let i = 0;
+  while(i < facts.length){
+    array1.push(facts[i] + '!!!');
+    i++;
+  }
+  return array1;
+}
+function iLoveTheBeatles(num){
+ var array = [];
+
+  do {
+    array.push('I love the Beatles!');
+    num++;
+  } while(num < 15);
+  return array;
+}
