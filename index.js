97f9c6a26a5427b7e5e025ad7ae309c96b5a5276
lite-closure-2800 <bri.barden@gmail.com>
2019-11-14 00:24:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+
+    var music = musicians[i];
+    var strum = instruments[i];
+     emptyArray.push(`${music} plays ${strum}`);
+  }
+return emptyArray
+}
+
+function johnLennonFacts(array) {
+var facts = [];
+  var i = 0;
+
+
+while (i < array.length) {
+  var plcHldr = array[i];
+  i++;
+  facts.push(`${plcHldr}!!!`);
+}
+    return facts;
+  }
+
+  function iLoveTheBeatles(number){
+    var beatles = [];
+    do {
+      beatles.push("I love the Beatles!")
+number++;
+    } while (number < 15);
+
+    return beatles;
+  }
