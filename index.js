af49bce0baeb9d1220afa6c36e345a80f930b7e5
confident-history-3187 <boris.matovic@gmail.com>
2019-11-30 09:07:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arr = []
+  for(var i=0;i< musicians.length;i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+return arr
+}
+
+function johnLennonFacts(facts){
+  var counter = 0;
+  while(counter < facts.length){
+    facts[counter] = `${facts[counter]}!!!`
+    counter++
+  }
+  return facts
+}
+
+
+function iLoveTheBeatles(num){
+  var arr = []
+  do {
+    arr.push('I love the Beatles!')
+    num--
+  } while (num >= 0);
+  return arr.length === 18 ? arr[0] : arr
+}
