c8cc74c8cba33d8f42e72580a0dd48c3d6ca1329
Gerald W Stelly <gstelly319@gmail.com>
2019-11-30 01:46:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var myArray = []
+  for (i = 0; i < musicians.length; i++) {
+    myArray.pop(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return myArray
+}
