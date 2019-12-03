b34a07d2d1a9379217d8269e83311edf13db168e
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-03 18:12:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var phrases = []
+
+  for (var i=0; i<musicians.length; i++) {
+    phrases[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+
+  return phrases
+}
+
+function johnLennonFacts (facts) {
+  var i = 0
+  var exclaimedFacts = []
+
+  while (i<facts.length) {
+    exclaimedFacts[i] = `${facts[i]}!!!`
+
+    i++
+  }
+
+  return exclaimedFacts
+}
+
+function iLoveTheBeatles (n) {
+  var love  = []
+  const i = n
+
+  do {
+    love[n-i] = "I love the Beatles!"
+    n++
+  } while (n<15)
+
+  return love
+}
