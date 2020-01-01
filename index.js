84b7386936961cc977d61937cb705bae5307c0e0
voluble-block-7446 <iagogomes80@gmail.com>
2020-01-01 23:49:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// add solution here
+var musicians=[];
+var instruments=[];
+function theBeatlesPlay(musicians,instruments){
+  var n=musicians.length;
+  var array1=[];
+  for(let i=0;i<n;i++){
+    array1.push(musicians[i]+" plays "+instruments[i]);
+  }
+  return array1;
+}
+var facts=[];
+function johnLennonFacts(facts){}
+var arr=[];
+function iLoveTheBeatles(arr){}
