e440912a21f6658ddcec575fe51f94573efa7c54
voluble-block-7446 <iagogomes80@gmail.com>
2020-01-02 00:08:03 +0000

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ function theBeatlesPlay(musicians,instruments){
   return array1;
 }
 var facts=[];
-function johnLennonFacts(facts){}
-var arr=[];
-function iLoveTheBeatles(arr){}
+function johnLennonFacts(facts){
+  var n=facts.length;
+  var array2=[];
+  for(let i=0;i<n;i++){
+    array2.push(facts[i]+"!!!");
+  }
+  return array2;
+}
+var num;
+function iLoveTheBeatles(num){
+  var arr=[];
+  var m=15-num;
+  var i=0;
+  do{
+    arr.push("I love the Beatles!")
+    i++;
+  }while(i<m && num<15);
+  return arr;
+}
