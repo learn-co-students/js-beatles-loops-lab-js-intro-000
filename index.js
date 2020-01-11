1728b854b20da685eae2778afcedacdb1df20a07
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-11 10:24:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+  for(var i = 0; i < 4; i++){
+    var sentence = `${musicians.shift()} plays ${instruments.shift()}`;
+    emptyArray.push(sentence);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(facts){
+  var excArray = [];
+  while(0 < facts.length){
+    var excSentence = `${facts.shift()}!!!`;
+    excArray.push(excSentence);
+  }
+  return excArray;
+}
+
+function iLoveTheBeatles(num){
+  var newArray = [];
+  do{
+    newArray.push('I love the Beatles!');
+  } while(newArray.length <= num && num < 15);
+  return newArray;
+}
