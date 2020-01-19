e89f3e4bfde58976a87f6932f61bbd85560bb92e
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-19 01:35:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay (array1, array2){var array3 = [];
+for (let i = 0; i < 4; i++) {array3.push(`${array1[i]} plays ${array2[i]}`)}
+
+return array3
+
+}
+function johnLennonFacts(array){var array3 = [];
+let countdown = array.length;
+
+while (countdown > 0) {array3.unshift(`${array[--countdown]}!!!`)}
+
+return array3
+}
+function iLoveTheBeatles(num) {var array1 = []; var counter = num;
+function incrementalVariable(){return ++counter}
+
+do {array1.push("I love the Beatles!")} while (incrementalVariable() < 15);
+
+return array1
+}
