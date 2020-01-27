d9cbcda7ffd7167b15ff23310929853b4ca74c1f
Bikram Sodhi <biksodhi@gmail.com>
2020-01-27 22:53:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var Kobe = []
+
+  for (let i = 0, l = musicians.length; i < l; i++){
+    Kobe.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return Kobe
+}
+
+function johnLennonFacts(factuals){
+  const shoutedFactuals = []
+  let i = 0
+  while (i < factuals.length){
+    shoutedFactuals.push(`${factuals[i]}!!!`)
+    i++
+  }
+  return shoutedFactuals
+}
+
+function iLoveTheBeatles(number){
+  const mamba = []
+
+  do{
+    mamba.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+  return mamba
+}
