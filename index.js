6d15e4fa54fff3a2f420634fc82052106de6383c
James Stamps Jr <jstampshere@gmail.com>
2020-02-11 03:06:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+ var array = []; 
+  for (var i = 0; i < musicians.length; i++) {
+  array.push(`${musicians[i]} plays ${instruments[i]}`);
+  
+       }
+return array
+}
+
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+  while (i < facts.length ) {
+    array.push(facts[i] + "!!!")
+    i++ ;
+      console.log(facts)
+  }
+  return array
+}
+
+
+
+function iLoveTheBeatles(num){
+  var array = [];
+do {
+    console.log("I love the Beatles!");
+    array.push("I love the Beatles!")
+    console.log(array)
+    num++;
+  } while (num < 15);
+    
+return array
+}
+
+
+
+
