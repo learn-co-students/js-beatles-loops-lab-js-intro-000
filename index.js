2904bcc6910ffc33049df4ab8d4e10321aed2e96
Christian Trevino <ctrevino1994@yahoo.com>
2020-02-23 19:45:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]// add solution helearn test
+
+function theBleatlesPlay(musicians, instruments) {
+  for (let i = 0, i < 3, i++){
+  answer.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+return answer
+}
