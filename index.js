5d0373f5d9c9450a3062ca53d53757beceda2c5a
interpreted-network-8638 <muratibuki@gmail.com>
2020-03-11 03:32:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// add solution here
+// add soluemption here
+function theBeatlesPlay(musicians,instruments){
+  var musiciansInstruments=[];
+  for(var i=0;i<musicians.length;i++)
+  {
+       musiciansInstruments[i]=`${musicians[i]} plays ${instruments[i]}`;
+  }
+return musiciansInstruments;
+}
+
+function johnLennonFacts(facts)
+{
+ var i=0
+ while(i<facts.length)
+ {
+   facts[i]=`${facts[i]}!!!`
+   i++;
+ }
+ return facts;
+}
+
+function iLoveTheBeatles(number)
+{
+  const emptyArray=[];
+  do{
+    emptyArray.push(`I love the Beatles!`);
+    number++;
+  }while(number<15)
+  return emptyArray;
+}
