839a5b1000f378570701799986f032486ef2afea
Sydney Michael <m13s19madden@gmail.com>
2020-03-11 19:50:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(mus, instr) {
+  var st = []
+  for (var i = 0; i < mus.length; i++) {
+    st.push(`${mus[i]} plays ${instr[i]}`)
+  }
+  return st
+}
+
+function johnLennonFacts(fac) {
+  var i = 0
+  var st = []
+  while (i < fac.length) {
+    st[i] = fac[i] + '!!!'
+    i++
+  }
+  return st
+}
+
+function iLoveTheBeatles(n) {
+  var st = []
+  do {
+    st.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+  return st
+}
