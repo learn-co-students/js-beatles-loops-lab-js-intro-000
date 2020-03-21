29393e8476d804001de7dfc38e59a2e606997495
Carly Lord <carly.lord@gmail.com>
2020-03-21 00:40:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var whatBeatlesPlay = []
+  for (var indx = 0; indx <= (musicians.length -1); indx++){
+    whatBeatlesPlay.push(`${musicians[indx]} plays ${instruments[indx]}`)
+  }
+  return whatBeatlesPlay
+}
+function johnLennonFacts(facts){
+  let excitedFacts = []
+  let indx = 0
+  while (indx <= (facts.length -1)){
+    excitedFacts.push(`${facts[indx]}!!!`)
+    indx++
+  }
+  return excitedFacts
+}
+function iLoveTheBeatles(num){
+  let beatlesChant = []
+  do {beatlesChant.push('I love the Beatles!')
+  num++
+} while (num<15)
+return beatlesChant
+}
