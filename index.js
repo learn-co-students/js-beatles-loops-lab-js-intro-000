9dfcf69137a59feacad239b7de9e71511c5d118b
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-25 21:45:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for(let i = 0; i < musicians.length; i++){
+    arr.push(musicians[i] + " plays " + instruments[i])
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts){
+  var newArr = [];
+  var i = 0;
+
+  while(i < facts.length){
+    var newFact = facts[i]+ "!!!" 
+    newArr.push(newFact);
+    i += 1;
+  }
+
+  return newArr;
+}
+
+function iLoveTheBeatles(num){
+  var fanArr = [];
+
+  let i = num;
+ 
+  do {
+    fanArr.push("I love the Beatles!");
+    i += 1;
+  } while (i < 15);
+
+  return fanArr;
+}
