9bebf73c623598651d39dddbe60a54c29e69f81d
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-03-31 07:41:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicans, instruments) {
+  var array = [];
+  for (let i = 0; i < musicans.length; i++) {
+    array.push(`${musicans[i]} plays ${instruments[i]}`);
+  } return array; 
+}
+function johnLennonFacts(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = array[i] + "!!!";
+  } return array;
+}
+
+/*
+function johnLennonFacts(array) {
+  var Newarray =[];
+  for (let i = 0; i < array.length; i++) {
+     Newarray.push(`${array[i]}!!!`);
+    // array[i] = array[i] + "!!!";
+  } return Newarray;
+}*/
+
+
+function iLoveTheBeatles(number) {
+  var NewArray = [];
+  do {
+    NewArray.push("I love the Beatles!");
+    number++;
+  } while ( number < 15 );
+return NewArray;
+}
