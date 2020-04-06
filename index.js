ac98cb0fb13dc207d82397c377aec5265e935462
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-06 18:05:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  while (i < facts.length) {
+      facts[i] = `${facts[i]}!!!`
+      i = i + 1;
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number) {
+  var newarray = [];
+  let i = number;
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  do {
+    newarray.push("I love the Beatles!");
+  } while (incrementVariable() < 15);
+  return newarray
+}
