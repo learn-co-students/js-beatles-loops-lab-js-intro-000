9d88e9015aa4f893a595a24fef0f4ef260bed0f3
Jasmine Lok <jasminellok@gmail.com>
2020-04-10 19:18:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  emptyArray= []
+  for (let i=0, i<musicians.length, i+=1) {
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return emptyArray
+}
