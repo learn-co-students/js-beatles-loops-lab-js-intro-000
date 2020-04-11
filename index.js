4da2797b41d31059d78735cd6d10b4b2a891b331
Illia Brytun <illiabrytun@gmail.com>
2020-04-11 19:25:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musician, instrument) {
+  var whoPlaysWhat=[]
+  for (let i=0; i<musician.length; i++) {
+    whoPlaysWhat.push(`${musician[i]} plays ${instrument[i]}`)
+  }
+  return whoPlaysWhat
+}
+
+function johnLennonFacts(facts) {
+  var updatedFacts = []
+  let i = 0
+  while (i<facts.length) {
+    updatedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return updatedFacts
+}
+
+function iLoveTheBeatles(num) {
+  var loveBeatlesToo = []
+  do {
+    loveBeatlesToo.push("I love the Beatles!")
+    num++
+  } while (num<15)
+  return loveBeatlesToo
+}
