35ec65ba73e3211e886eca138b3cf20c52f2e525
Robert Howden <robert.howden@current-rms.com>
2020-04-21 08:13:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 theBeatlesPlay = (m, ins) => {
-  result = []
-  for (let i = 0, i < m.length, i++) {
-    result.push(`${m[0]} plays ${ins[i]} `)
+  var result = []
+  for (let i = 0; i < m.length; i++) {
+    result.push(`${m[i]} plays ${ins[i]}`)
   }
+
+  return result
+}
+
+function johnLennonFacts(a) {
+  var result = []
+
+  let i = 0
+  while (i < a.length) {
+    result.push(a[i] + '!!!')
+    i++
+  }
+
+  return result
+}
+
+function iLoveTheBeatles(n) {
+  var result = []
+
+  do {
+    result.push('I love the Beatles!')
+    n++
+  }while(n < 15)
 
   return result
 }
