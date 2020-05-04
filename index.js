2fdeefb2b799a024e9047a93270d5098f095663d
Corey Lynch <bearinawolfpack@gmail.com>
2020-05-04 17:08:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const arr = [];
+  const music = musicians.length;
+  const instr = instruments.length;
+
+  for (let num = 0; num < music; num++) {
+    arr.push(musicians[num] + " plays " + instruments[num]);
+  }
+  return arr;
+}
+
+// ---------------------------------------------------------------------------
+
+function johnLennonFacts(array) {
+  var length = array.length;
+  var num = 0;
+  var arr = [];
+
+  while(length > num){
+    arr.push(array[num] + "!!!");
+    num += 1;
+  }
+  return arr;
+}
+
+// ---------------------------------------------------------------------------
+
+function iLoveTheBeatles(num) {
+  var arr = [];
+  var n = 0;
+
+  if(num > 15) {
+    num = 0;
+  }
+
+  function incrementVariable() {
+    n = n + 1;
+    return n;
+  }
+
+  do {
+    arr.push("I love the Beatles!")
+  } while (incrementVariable() <= num);
+return arr;
+}
