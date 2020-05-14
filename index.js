76a571531e7e8215291ec34e1d3c4f5b2ae152b4
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-05-14 18:33:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments)
+{
+  var theBeatles = [];
+
+  for(let i = 0; i < musicians.length; i++)
+  {
+    var musicianInfo = musicians[i] + " plays " + instruments[i]
+
+    theBeatles.push(musicianInfo)
+  }
+  return theBeatles;
+}
+
+function johnLennonFacts(facts)
+{
+  var johnLennon = [];
+  var n = 0;
+
+  while ( n<facts.length)
+  {
+    johnLennon.push(facts[n] + "!!!")
+    n++
+  }
+
+  return johnLennon
+
+}
+
+
+function iLoveTheBeatles(num)
+
+{
+  var loveTheBeatles = [];
+
+
+  do {
+    loveTheBeatles.push("I love the Beatles!")
+  } while (num>=7);
+  num ++
+
+  return loveTheBeatles
+}
