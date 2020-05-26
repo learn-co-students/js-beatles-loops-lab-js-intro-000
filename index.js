d0c642311681644ae1867c61132a81b8356988e8
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-26 19:26:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+  var empty = [];
+  for (var i = 0; i < arrayMusicians.length; i++) {
+    empty.push(arrayMusicians[i] +" " + "plays" + " " + arrayInstruments[i]);
+  }
+  return empty;
+}
+function johnLennonFacts(facts) {
+  var empty = [];
+  var i = 0;
+  while (empty.length < facts.length) {
+    empty.push(facts[i] + "!!!");
+    i++;
+  }
+  return empty;
+}
+
+function iLoveTheBeatles(num) {
+  var empty = [];
+  do {empty.push("I love the Beatles!"); num += 1}
+  while (num < 15);
+  return empty;
+}
