6d0c457c19deb6565f16147750e3c5eca9e22400
Daniel A Koontz <dak.7997@gmail.com>
2020-06-14 20:10:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var theyPlay = []
+
+  for (let i = 0; i < musicians.length; i++) {
+    theyPlay.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return theyPlay
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+
+  while (i < facts.length) {
+    facts[i] += "!!!"
+    i++
+  }
+
+  return facts
+}
+
+function iLoveTheBeatles(n) {
+  var love = []
+
+  do {
+    love.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+
+  return love
+}
