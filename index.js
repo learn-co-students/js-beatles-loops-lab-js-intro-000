72d745227647ac2e23248b0e3fee819060cbbe01
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-30 01:54:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let output = [];
+
+  for(let i = 0 ; i < musicians.length ; i++){
+    output.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return output;
+}
+
+function johnLennonFacts(facts){
+  let count = 0;
+  let output = [];
+
+  while (count < facts.length){
+    output.push(`${facts[count]}!!!`)
+    count++
+  }
+  return output
+}
+
+function iLoveTheBeatles(num){
+  let output = [];
+
+  do {
+    output.push('I love the Beatles!')
+    num++
+  } while (num < 15)
+
+  return output;
+}
