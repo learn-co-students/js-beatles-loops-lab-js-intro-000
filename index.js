498f5d27a59d1e728fc948977bb93c87bded120b
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-28 22:33:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  var len = musicians.length;
+  for(let i = 0; i < len; i++) {
+    arr[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return arr;
+}
+function johnLennonFacts(facts) {
+  var arr = [];
+  var len = facts.length;
+  for(let i = 0; i < len; i++) {
+    arr[i] = facts[i] + "!!!";
+  }
+  return arr;
+}
+function iLoveTheBeatles(number) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    number++;
+  } while(number < 15)
+  return arr;
+}
