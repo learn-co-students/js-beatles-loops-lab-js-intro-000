9b566f02e375ce87e42b6bae25749ad5906bd3e9
reflective-bell-5160 <mvu7@uic.edu>
2020-08-09 21:08:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var new_array = []
+  for (var i = 0; i < musicians.length; i++){
+    var string = musicians[i] + " plays " +instruments[i];
+    new_array.push(string);
+  }
+
+
+  return new_array;
+
+};
+
+function johnLennonFacts(arrayFacts){
+  var new_array = []
+  for(var i = 0; i < arrayFacts.length; i++){
+    var new_string = arrayFacts[i] + "!!!";
+    new_array.push(new_string);
+  }
+  return new_array;
+}
+
+function iLoveTheBeatles(num){
+  var new_array = [];
+  do {
+    new_array.push("I love the Beatles!");
+    num += 1
+  } while (num < 15);
+  return new_array;
+}
