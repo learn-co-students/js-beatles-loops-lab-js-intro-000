39cfdcb421da05c7fc54cf0bd6f3cd3226a234ed
Ada Huang <ada10huang@gmail.com>
2020-08-14 23:47:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+let array = []
+
+let theBeatlesPlay = (musicians, instruments) => {
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  } return array
+}
+
+const facts = ["He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"];
+
+let i = 0
+
+let johnLennonFacts = facts => {
+  while (i < facts.length) {
+    facts.push(`${facts[i]}` + "!!!")
+    i += 1
+  } return facts
+}
