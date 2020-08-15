58be34cb7675704c136ba2e6b76799eaa4e66fce
Ada Huang <ada10huang@gmail.com>
2020-08-15 02:13:50 +0000

    Done.

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ let theBeatlesPlay = (musicians, instruments) => {
   } return array
 }
 
-const facts = ["He was the last Beatle to learn to drive",
-"He was never a vegetarian",
-"He was a choir boy and boy scout",
-"He hated the sound of his own voice"];
-
 let i = 0
 
 let johnLennonFacts = facts => {
   while (i < facts.length) {
-    facts.splice(i, 1, `${facts[i]!!!}`);
+    let facts[i] = facts.splice(i, 1, facts[i] + "!!!");
     i += 1
   } return facts
 }
