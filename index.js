7aaa91d25001ad3c470cc100216216eac14969f6
Ada Huang <ada10huang@gmail.com>
2020-08-15 02:05:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // add solution here
-let array = []
-
 let theBeatlesPlay = (musicians, instruments) => {
+  let array = [];
   for (let i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   } return array
@@ -16,7 +15,7 @@ let i = 0
 
 let johnLennonFacts = facts => {
   while (i < facts.length) {
-    return facts(){`${facts[i]}!!!`}
+    facts.splice(i, 1, `${facts[i]!!!}`);
     i += 1
   } return facts
 }
