6162b2d8b04ab4200106fb9419ef3135784467f8
Ada Huang <ada10huang@gmail.com>
2020-08-17 21:39:22 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ let i = 0
 
 let johnLennonFacts = facts => {
   while (i < facts.length) {
-    facts() {facts.splice(i, 1, facts[i] + "!!!")};
+    facts.splice(i, 1, `${facts[i]}!!!`);
     i += 1
   } return facts
 }
+
+let iLoveTheBeatles = num => {
+  let array = []
+  do {
+    array.push("I love the Beatles!");
+    num += 1
+  } while (num < 15)
+} return array
