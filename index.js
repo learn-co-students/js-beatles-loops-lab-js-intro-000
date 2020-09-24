32d92c16dc397af95eb114923166f7eba7c2ec58
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 21:37:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function theBeatlesPlay(musicians, instruments) {
   var i = []
-  for (let n = 0; n<musicians.length; n++) {
-  i.push(`${musicians[n]} plays ${instruments[n]}`)
-  }
+  for (let n = 0; n < musicians.length; n++) {
+    i.push(`${musicians[n]} plays ${instruments[n]}`);
+    }
   return i
 }
 
 function johnLennonFacts(facts) {
-  let i = 0
-let n = []
+  var i = 0
+  var n = []
   while (facts.length > i) {
-  n.push(`${facts[i]}!!!`)
+    n.push(`${facts[i]}!!!`)
         i+=1
-}
+  }
   return n
 }
 
 function iLoveTheBeatles(num) {
-let n = []
+  var i = []
   do {
-  n.push("I love the Beatles!")
-        num++
-}
-while (num<15);
-return n
+  i.push("I love the Beatles!")
+      num++
+  }
+  while (num < 15);
+  return i
 }
