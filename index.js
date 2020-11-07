9e335d5fcebb37cf2cdaec8171b5800ef88c8740
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 01:54:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let arr = [];
+  for(let i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts) {
+  let counter = 0;
+  let modifiedFacts = [];
+  while(counter < facts.length) {
+    modifiedFacts.push(`${facts[counter]}!!!`);
+    counter++;
+  }
+  return modifiedFacts;
+}
+
+function iLoveTheBeatles(num) {
+  let arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    num++;
+  } while(num < 15);
+  return arr;
+}
