dfc3decd578efaf87b19bcb81ca6fa25ed5dc58a
Chris <chris.mahfouz@hotmail.com>
2020-11-25 15:31:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-// add solution here
 function theBeatlesPlay(musicians,instruments){
   var instrumentsPlayed=[]
-  for (let num = 0;num < musicians.length;num +=1){
-    instrumentsPlayed.push(`${musicians[num]} plays ${instruments[num]}`)
+  for(let num = 0; num <musicians.length;num += 1){
+   instrumentsPlayed.push(`${musicians[num]} plays ${instruments[num]}`)
   }
   return instrumentsPlayed
 }
-
-
-// create a function johnLennonFacts it accepts one argument, an array of facts
-function johnLennonFacts(facts) {
-  let lennonFacts = [];
-
-  // use a while loop over the facts array and add "!!!" to the end of every fact.
-  let index = 0;
-  while (index<facts.length) {
-    lennonFacts.push(`${facts[index]}!!!`);
-    index++;
+function johnLennonFacts(facts){
+var arrayFacts=[]
+  let num=0
+  while(num < facts.length){
+    arrayFacts.push(`${facts[num]}!!!`)
+    num+=1
   }
-
-  // return an array of strings with exclamation points.
-  return lennonFacts;
+  return arrayFacts
 }
-// create a function iLoveTheBeatles which accepts a number as a parameter
-function iLoveTheBeatles(num) {
-  // create a variable that stores an empty array.
-  let arr = [];
-
-  // implement a do-while loop inside the function that adds "I love the Beatles!" to the empty array.
-  // the loop should increment the number passed as a parameter.
-  // the condition of the loop should check that the parameter number is less than 15
-  do {
-    arr.push("I love the Beatles!");
-    num++;
-  } while (num < 15);
-
-  // return the array of strings.
-  return arr
+function iLoveTheBeatles(number){
+  var arrayNumber=[]
+  do{arrayNumber.push("I love the Beatles!")
+  number += 1
+  }
+  while (number <15
+  )
+  return arrayNumber
 }
