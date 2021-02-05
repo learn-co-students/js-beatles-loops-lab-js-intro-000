53ba5a4d3d222737509fb840489359f4b40a412d
Chris <chris.mahfouz@hotmail.com>
2021-02-05 15:21:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 function theBeatlesPlay(musicians,instruments){
-  var instrumentsPlayed=[]
-  for(let num = 0; num <musicians.length;num += 1){
-   instrumentsPlayed.push(`${musicians[num]} plays ${instruments[num]}`)
+  var instrumentsplayed=[]
+  for(let i=0;i <musicians.length;i++){
+    instrumentsplayed.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  return instrumentsPlayed
+  return instrumentsplayed
 }
 function johnLennonFacts(facts){
-var arrayFacts=[]
-  let num=0
-  while(num < facts.length){
-    arrayFacts.push(`${facts[num]}!!!`)
-    num+=1
+  var newfact=[]
+  let i=0
+  while(i<facts.length){
+    newfact.push(`${facts[i]}!!!`)
+    i++
   }
-  return arrayFacts
-}
-function iLoveTheBeatles(number){
-  var arrayNumber=[]
-  do{arrayNumber.push("I love the Beatles!")
-  number += 1
-  }
-  while (number <15
-  )
-  return arrayNumber
+  return newfact
 }
