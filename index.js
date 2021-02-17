5a7cc2938701957ce4c24eb17634f088b005e916
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2021-02-17 15:27:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+    var pairs = [];
+    for (let i = 0; i < musicians.length; i++) {
+        pairs.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+    return pairs;
+};
+
+function johnLennonFacts(John) {
+   var punctuation = [];
+   let n = 0;
+   while (John.length > n) {
+      punctuation.push(`${John[n]}!!!`)
+      n += 1
+   }
+   return punctuation;
+};
+
+function iLoveTheBeatles(n) {
+   var beatles = [];
+   do { 
+   	beatles.push(`I love the Beatles!`)
+    n += 1;
+   } while ( n < 15);
+   return beatles;
+};
