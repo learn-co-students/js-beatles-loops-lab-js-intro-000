540dff6b04c2b8c124da008ddd8ea4a325916c77
Anthony Johnson <ajohnso2020@gmail.com>
2021-03-21 23:14:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+var array = [];
+for (let i = 0; i < musicians.length; i++) {
+array.push(`${musicians[i]} plays ${instruments[i]}`);// add solution here
+}
+return array;
+}
+
+function johnLennonFacts(facts){
+var facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+var array =[];
+let i = 0;
+while(i < facts.length){
+array.push(`${facts[i]!!!`);
+i++;
+}
+return facts;
+}
