04d99f43b93fb50df4a0ae03c4967c33c66b181b
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-30 16:33:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  for (let i = 0; i < 4; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+    //console.log(newArray);
+  }
+  //console.log(newArray);
+  return newArray;
+}
+
+function johnLennonFacts(array) {
+  var newArray = []
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[i] + "!!!");
+  }
+  console.log(newArray);
+  return newArray;
+}
+
+function iLoveTheBeatles(num) {
+  var newArray = [];
+  if (num < 15) {
+    for (let i = 0; i <= num; i++) {
+      newArray.push("I love the Beatles!");
+    }
+  }
+  else {
+    newArray.push("I love the Beatles!");
+  }
+  return newArray;
+}
